4cbeff535b55c148b0794887fb4adf29a0b7cc68
Damian Dzierzak <damian.dzierzak@miquido.com>
2018-07-17 15:30:47 +0200

    Remove redundant code in logger

--- revoltsdk/src/main/java/com/miquido/revoltsdk/internal/log/RevoltLogger.kt
+++ revoltsdk/src/main/java/com/miquido/revoltsdk/internal/log/RevoltLogger.kt
@@ -19,65 +19,69 @@ internal object RevoltLogger {
 
     fun e(message: String) {
         if (shouldShowLog(ERROR)) {
-            Log.e(TAG, "$message, currentThread: ${Thread.currentThread().name}")
+            Log.e(TAG, formatLog(message))
         }
     }
 
     fun e(message: String, throwable: Throwable) {
         if (shouldShowLog(ERROR)) {
-            Log.e(TAG, "$message, currentThread: ${Thread.currentThread().name}", throwable)
+            Log.e(TAG, formatLog(message), throwable)
         }
     }
 
     fun w(message: String) {
         if (shouldShowLog(WARN)) {
-            Log.w(TAG, "$message, currentThread: ${Thread.currentThread().name}")
+            Log.w(TAG, formatLog(message))
         }
     }
 
     fun w(message: String, throwable: Throwable) {
         if (shouldShowLog(WARN)) {
-            Log.w(TAG, "$message, currentThread: ${Thread.currentThread().name}", throwable)
+            Log.w(TAG, formatLog(message), throwable)
         }
     }
 
     fun i(message: String) {
         if (shouldShowLog(INFO)) {
-            Log.i(TAG, "$message, currentThread: ${Thread.currentThread().name}")
+            Log.i(TAG, formatLog(message))
         }
     }
 
     fun i(message: String, throwable: Throwable) {
         if (shouldShowLog(INFO)) {
-            Log.i(TAG, "$message, currentThread: ${Thread.currentThread().name}", throwable)
+            Log.i(TAG, formatLog(message), throwable)
         }
     }
 
     fun d(message: String) {
         if (shouldShowLog(DEBUG)) {
-            Log.d(TAG, "$message, currentThread: ${Thread.currentThread().name}")
+            Log.d(TAG, formatLog(message))
         }
     }
 
     fun d(message: String, throwable: Throwable) {
         if (shouldShowLog(DEBUG)) {
-            Log.d(TAG, "$message, currentThread: ${Thread.currentThread().name}", throwable)
+            Log.d(TAG, formatLog(message), throwable)
         }
     }
 
     fun v(message: String) {
         if (shouldShowLog(VERBOSE)) {
-            Log.v(TAG, "$message, currentThread: ${Thread.currentThread().name}")
+            Log.v(TAG, formatLog(message))
         }
     }
 
     fun v(message: String, throwable: Throwable) {
         if (shouldShowLog(VERBOSE)) {
-            Log.v(TAG, "$message, currentThread: ${Thread.currentThread().name}", throwable)
+            Log.v(TAG, formatLog(message), throwable)
         }
     }
 
     private fun shouldShowLog(revoltLogLevel: RevoltLogLevel): Boolean {
         return logLevel >= revoltLogLevel
     }
+
+    private fun formatLog(message: String) = "[${threadName()}] $message"
+
+    private fun threadName() = Thread.currentThread().name.padEnd(12).substring(0, 12)
 }
